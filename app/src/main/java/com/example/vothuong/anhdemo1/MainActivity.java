57fcc9b5202f87a381275551fa96f22a09301d78
apham19 <apham19@student.gsu.edu>
2017-04-01 23:26:25 -0400

    v4.1.17

--- app/src/main/java/com/example/vothuong/anhdemo1/MainActivity.java
+++ app/src/main/java/com/example/vothuong/anhdemo1/MainActivity.java
@@ -1,29 +1,62 @@
 package com.example.vothuong.anhdemo1;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.example.vothuong.anhdemo1.activity.Animation_Activity;
 import com.example.vothuong.anhdemo1.activity.AnimatorActivity;
 import com.example.vothuong.anhdemo1.activity.TimerActivity;
 import com.example.vothuong.anhdemo1.activity.toA_Activity;
+import com.example.vothuong.anhdemo1.activity.toB_Activity;
+import com.example.vothuong.anhdemo1.activity.toC_Activity;
+import com.example.vothuong.anhdemo1.activity.toD_Activity;
 import com.example.vothuong.anhdemo1.bean.Book;
 import com.example.vothuong.anhdemo1.dialog.CustomDialog;
 import com.example.vothuong.anhdemo1.dialog.Main_Custom_Dialog;
 import com.example.vothuong.anhdemo1.util.UtilLog;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnTouchListener{
 
     private ImageButton bt1;
     private ImageButton bt3;
     private ImageButton bt_act;
 
+    private GestureDetector mGestureDetector;
+    @BindView(R.id.main_fl)FrameLayout fl;
+    @BindView(R.id.listOfActivity)
+    LinearLayout ll;
+    int count=0;
+
+    @OnClick(R.id.profile)
+    void show_profile(){
+        if(count==0){
+            ObjectAnimator animator = ObjectAnimator.ofFloat(ll, "translationX", 0, 950, 950,950,950); //translationY
+            animator.setDuration(1000);
+            animator.start();
+            count++;
+        }
+        else{
+            ObjectAnimator animator = ObjectAnimator.ofFloat(ll, "translationX", 950, 0, 0,0,0); //translationY
+            animator.setDuration(1000);
+            animator.start();
+            count=0;
+        }
+
+    }
 
     @OnClick(R.id.custom)
     void customClick(){
@@ -48,7 +81,68 @@ public class MainActivity extends BaseActivity {
         dialog.show();
     }
 
+    @OnClick(R.id.p_viewPager)
+    public void to_ViewPager(){
+        Intent intent = new Intent(this,ViewPagerActivity.class);
+        intent.putExtra("key", "value");
+        Bundle bundle = new Bundle();
+        bundle.putInt("Integer", 12345);
+        Book book = new Book();
+        book.setName("Android");
+        book.setAuthor("Anh");
+        bundle.putSerializable("book", book);
+        intent.putExtras(bundle);
+        startActivityForResult(intent,1);
+    }
 
+    @OnClick(R.id.p_ListView)
+    public void listViewActivity(){
+        toActivity(ListViewActivity.class);
+    }
+
+    @OnClick(R.id.p_Animation)
+    public void animationActivity(){
+        toActivity(Animation_Activity.class);
+    }
+
+    @OnClick(R.id.p_Animator)
+    public void animatorActivity(){
+        toActivity(AnimatorActivity.class);
+    }
+
+    @OnClick(R.id.p_timer)
+    public void timerActivity(){
+        toActivity(TimerActivity.class);
+    }
+
+    @OnClick(R.id.p_dialog)
+    public void dialogActivity(){
+        toActivity(DialogActivity.class);
+    }
+
+    @OnClick(R.id.p_toA)
+    public void aActivity(){
+        toActivity(toA_Activity.class);
+    }
+
+    @OnClick(R.id.p_toB)
+    public void bActivity(){
+        toActivity(toB_Activity.class);
+    }
+
+    @OnClick(R.id.p_toC)
+    public void cActivity(){
+        toActivity(toC_Activity.class);
+    }
+    @OnClick(R.id.p_toD)
+    public void dActivity(){
+        toActivity(toD_Activity.class);
+    }
+
+    @OnClick(R.id.animator)
+    public void toAnim() {
+        toActivity(AnimatorActivity.class);
+    }
 
     @OnClick(R.id.timer)
     void buttonTimerClick(){
@@ -77,6 +171,8 @@ public class MainActivity extends BaseActivity {
         initialView();
         initialListener();
         ButterKnife.bind(this);
+        mGestureDetector = new GestureDetector(this, new simpleGestureListener());
+        fl.setOnTouchListener(this);
     }
 
     private void initialView(){
@@ -104,7 +200,7 @@ public class MainActivity extends BaseActivity {
                 bundle.putInt("Integer", 12345);
                 Book book = new Book();
                 book.setName("Android");
-                book.setAuthor("Viral");
+                book.setAuthor("Anh");
                 bundle.putSerializable("book", book);
                 intent.putExtras(bundle);
                 startActivityForResult(intent,1); //can pass infomration through intent
@@ -165,5 +261,58 @@ public class MainActivity extends BaseActivity {
         Log.d("testD","Toast");
     }
 
+    public boolean onTouch(View v, MotionEvent event){
+        return mGestureDetector.onTouchEvent(event);
+
+    }
+    private class simpleGestureListener extends
+            GestureDetector.SimpleOnGestureListener{
+
+        public boolean onDown(MotionEvent e){
+//            UtilLog.logD("MyGesture", "onDown");
+            toastShort("onDown");
+            show_profile();
+            return true;
+        }
+        public void onShowPress(MotionEvent e){
+//            UtilLog.logD("MyGesture", "onShowPress");
+            toastShort("onShowPress");
+        }
+        public void onLongPress(MotionEvent e){
+//            UtilLog.logD("MyGesture", "onLongPress");
+            toastShort("onLongPress");
+        }
+        public boolean onSingleTapUp(MotionEvent e){
+//            UtilLog.logD("myGesture", "onSingleTapUp");
+            toastShort("onSingleTapUp");
+            return true;
+        }
+        public boolean onSingleTapConfirmed(MotionEvent e){
+//            UtilLog.logD("myGesture", "onSingleTapUp");
+            toastShort("onSingleTapConfirmed");
+            return true;
+        }
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY){
+            UtilLog.logD("myGesture", "onScroll:" + (e2.getX() - e1.getX()) + " " + distanceX);
+            toastShort("onScroll");
+            return true;
+        }
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY){
+            //           UtilLog.logD("myGesture", "onFling");
+            toastShort("onFling");
+            return true;
+        }
+        public boolean onDoubleTap(MotionEvent e){
+            //           UtilLog.logD("myGesture", "onDoubleTap");
+            //           toastShort("onDoubleTap");
+            return true;
+        }
+        public boolean onDoubleTapEvent(MotionEvent e){
+            //           UtilLog.logD("myGesture", "onDoubleTapEvent");
+            //           toastShort("onDoubleTapEvent");
+            return true;
+        }
+
+    }
 
 }
